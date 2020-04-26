909371bbf2450f24be14ba3cc9ef993cf12637fc
yan043 <mahdian043@gmail.com>
2020-04-26 20:39:24 +0800

    Percabangan dengan condition False

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,13 +2,13 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 53000;
-        double uangDiDompet = 25000;
+        double totalBelanja = 25000;
+        double uangDiDompet = 50000;
 
         if(uangDiDompet<totalBelanja) {
             System.out.println("Uang kurang, kurangi jajan anda");
         }else{
-            System.out.println("Uang cukup, selamat menikmati");
+            System.out.println("Uang cukup, berelaan lah");
         }
     }
 }
