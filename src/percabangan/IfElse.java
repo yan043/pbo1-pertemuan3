e94d3342716f60e373b763d28cb8d87b67b4810d
yan043 <mahdian043@gmail.com>
2020-04-26 20:54:46 +0800

    Percabangan dengan condition2 True

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -4,11 +4,15 @@ public class IfElse {
     public static void main(String[] args) {
         double totalBelanja = 25000;
         double uangDiDompet = 50000;
+        // noted pak, di soal totalBelanja = 53000 & uangDiDompet = 25000
 
         if(uangDiDompet<totalBelanja) {
             System.out.println("Uang kurang, kurangi jajan anda");
+        }else if(uangDiDompet>totalBelanja){
+            double angsul = uangDiDompet - totalBelanja;
+            System.out.println("Uang cukup, angsul : " + angsul);
         }else{
-            System.out.println("Uang cukup, berelaan lah");
+            System.out.println("Uang pas.. Uwuuu...");
         }
     }
 }
