dfb1a5cc2c2e814b462cdaec50d0638946ede4e9
yan043 <mahdian043@gmail.com>
2020-04-26 20:14:55 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorBinary {
+
+    // operator (=) sebagai assigment/penugasan
+    // menempatkan nilai kedalam variable
+    // baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Mahdian";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+}
